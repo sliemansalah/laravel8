e716ab0deab74ac77095bcfd174e760cef7ca838
sliemansalah <slieman2001@hotmail.com>
2021-09-30 11:59:22 +0300

    up.

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -3,14 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/app.css"  />
         <title>My blog</title>
     </head>
     <body>
          <h1 class="text-center primaryColor">   Welcome to blog course </h1>
     </body>
 </html>
-
-<style>
-    .text-center {text-align: center}
-    .primaryColor {color:blue;}
-</style>
