f89bf3085b94e56a9412cd05a3a8a362b05cf081
sliemansalah <slieman2001@hotmail.com>
2021-09-30 11:56:53 +0300

    up.

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>My blog</title>
+    </head>
+    <body>
+         <h1 class="text-center primaryColor">   Welcome to blog course </h1>
+    </body>
+</html>
+
+<style>
+    .text-center {text-align: center}
+    .primaryColor {color:blue;}
+</style>
